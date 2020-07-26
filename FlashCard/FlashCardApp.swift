db4b27b74ebc7a820dc75b56bae607a44919960c
Jimmy Yeung <jiyeung@ebay.com>
2020-07-26 18:48:33 +0800

    Initial Commit

---
+++ FlashCard/FlashCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashCardApp.swift
+//  FlashCard
+//
+//  Created by Jimmy, Yeung on 26/7/2020.
+//
+
+import SwiftUI
+
+@main
+struct FlashCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
